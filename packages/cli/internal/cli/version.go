acce5d9c913dfef90ff0c060d35a9cdbc8a883c7
endalk200 <eb808826@gmail.com>
2026-04-25 16:30:21 +0300

    chore: adds initial version of bw CLI v2 with full config

---
+++ packages/cli/internal/cli/version.go
@@ -0,0 +1,26 @@
+package cli
+
+const defaultVersion = "2.0.0-beta.1"
+
+var (
+	version = defaultVersion
+	commit  = "unknown"
+	date    = "unknown"
+	builtBy = "source"
+)
+
+type BuildInfo struct {
+	Version string
+	Commit  string
+	Date    string
+	BuiltBy string
+}
+
+func BuildInfoFromLinker() BuildInfo {
+	return BuildInfo{
+		Version: version,
+		Commit:  commit,
+		Date:    date,
+		BuiltBy: builtBy,
+	}
+}
